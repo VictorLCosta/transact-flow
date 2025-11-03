238dfff8f445260a9d39b11f0df52514d7985dfa
VictorLCosta <victorlc2019@outlook.com>
2025-11-03 10:21:50 -0300

    feat: add middleware for XSS protection and update dependencies for security enhancements

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
+import compression from "compression";
+import cors from "cors";
 import express from "express";
+import helmet from "helmet";
+import xss from "middlewares/xss";
 
 const app = express();
+
+app.use(helmet());
+
+app.use(express.json());
+
+app.use(xss());
+
+app.use(compression());
+
+app.use(cors());
+app.options("/{*any}", cors());
 
 export default app;
