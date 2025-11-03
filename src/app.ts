df0e55159a1a032d33745d6a23aeec8e0da3cf65
VictorLCosta <victorlc2019@outlook.com>
2025-11-03 11:25:33 -0300

    feat: integrate morgan and winston for enhanced logging and request handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import compression from "compression";
+import config from "config";
+import morgan from "config/morgan";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import xss from "middlewares/xss";
 
 const app = express();
+
+if (config.NODE_ENV !== "test") {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
 
 app.use(helmet());
 
---
+++ src/config/morgan.ts
@@ -0,0 +1,27 @@
+import config from "config";
+import morgan from "morgan";
+
+import logger from "./logger";
+
+import type { Response } from "express";
+
+morgan.token("message", (_, res: Response) => res.locals.errorMessage || "");
+
+const getIpFormat = () => (config.NODE_ENV === "production" ? ":remote-addr - " : "");
+const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms`;
+const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - message: :message`;
+
+export const successHandler = morgan(successResponseFormat, {
+  skip: (_, res) => res.statusCode >= 400,
+  stream: { write: (message) => logger.info(message.trim()) },
+});
+
+export const errorHandler = morgan(errorResponseFormat, {
+  skip: (_, res) => res.statusCode < 400,
+  stream: { write: (message) => logger.error(message.trim()) },
+});
+
+export default {
+  successHandler,
+  errorHandler,
+};
