09572a940578628787e222d3cddfb762bd9d0ab0
VictorLCosta <victorlc2019@outlook.com>
2025-11-03 15:10:41 -0300

    feat: implement exclude and pick utility functions for object manipulation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,36 @@
+import logger from "config/logger";
+
 import app from "./app";
 import config from "./config";
 
-app.listen(config.PORT, () => {
-  console.log("Server is running on port " + config.PORT);
+import type { Server } from "http";
+
+const server: Server = app.listen(config.PORT, () => {
+  logger.info("Server is running on port " + config.PORT);
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info("Server closed");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error: unknown) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM received");
+  if (server) {
+    server.close();
+  }
 });
